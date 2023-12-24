f559e87a1117405f55c083d9b01aefb368ec2fc4
Nithish Kumar <98147654+NithishKumar1123@users.noreply.github.com>
2023-12-24 17:52:57 +0530

    Update config.py

--- application/config.py
+++ application/config.py
@@ -2,18 +2,25 @@ import os
 current_directory = os.path.abspath(os.path.dirname(__file__))
 
 class Config():
-    DEBUG = False
-    SQLITE_DB_DIR = None
-    SQLALCHEMY_DATABASE_URI = None
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    WTF_CSRF_ENABLED = False
+    SQLITE_DB_DIR = os.path.join(current_directory, '../db_directory')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(SQLITE_DB_DIR, 'MarketMateDB.sqlite3')
+    SECRET_KEY = 'hdyg47f8fnsyvbs'
+    SECURITY_PASSWORD_HASH = 'bcrypt'
+    SECURITY_PASSWORD_SALT = 'defefygrs678bfr4'
+    SECURITY_REGISTERABLE = True
+    SECURITY_SEND_REGISTER_EMAIL = False
+    SECURITY_UNAUTHORIZED_VIEW = None
+    WTF_CSRF_ENABLED = True
     SECURITY_TOKEN_AUTHENTICATION_HEADER = 'Authentication-Token'
     CELERY_BROKER_URL = "redis://localhost:6379/1"
     CELERY_RESULT_BACKEND = "redis://localhost:6379/2"
+    CELERY_TIMEZONE = "Asia/Kolkata"
+    CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
     REDIS_URL = "redis://localhost:6379"
     CACHE_TYPE = "RedisCache"
     CACHE_REDIS_HOST = "localhost"
     CACHE_REDIS_PORT = 6379
+    DEBUG = True
     
 class LocalDevelopmentConfig(Config):
     SQLITE_DB_DIR = os.path.join(current_directory, '../db_directory')
